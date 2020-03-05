a2d85fc001bf98901f710b5e022137ae14081bce
byron <st06266@gmail.com>
2020-03-05 11:11:02 -0600

    done son!

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+
+
+
+app.get("/", function(req, res){
+    res.render("index");
+})
+
+
+
+
+
+
+app.listen(3000, function(){
+    console.log("server started on port 3000");
+});
