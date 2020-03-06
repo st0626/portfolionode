7eab12f291da84e195218058fd2eeceed52ff56c
byron <st06266@gmail.com>
2020-03-06 07:53:01 -0600

    updated the port variable for heroku

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
+const PORT = 3000;
 
 
 const app = express();
@@ -22,6 +23,6 @@ app.get("/", function(req, res){
 
 
 
-app.listen(3000, function(){
+app.listen(PORT, function(){
     console.log("server started on port 3000");
 });
