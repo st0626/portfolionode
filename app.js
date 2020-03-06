67a78f92b40e52868bc76003ea2c657a08b42af7
byron <st06266@gmail.com>
2020-03-05 21:03:55 -0600

    trying to get express to serve static files
    I can't get express to serve static files

--- app.js
+++ app.js
@@ -5,13 +5,16 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.set("view engine", "ejs");
-app.use(express.static("public"));
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
 
 
 app.get("/", function(req, res){
     res.render("index");
-})
+});
+
+
 
 
 
