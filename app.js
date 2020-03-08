7c73b5c55b74fc18745b91cfca77b7bf971ef4c5
byron <st06266@gmail.com>
2020-03-07 18:20:54 -0600

    added nodemailer

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
 
 
+//Nodemailer stuff
+
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'st06266@gmail.com',
+      pass: 'dkdywqgbixfebeyl'
+    }
+  });
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////
 
 const app = express();
 
@@ -20,7 +41,33 @@ app.get("/projects", function(req, res){
     res.render("projects");
 });
 
+app.post('/', function(req, res){
+    let email = req.body.email;
+    let desc = req.body.description;
+    
+    console.log({
+    email: email,
+    description: desc
+    });
 
+    let mailOptions = {
+        from: email,
+        to: 'st06266@gmail.com',
+        subject: 'Nodemailer: from ' + email,
+        text: desc
+    }
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if(error) {
+            console.log(error);
+        } else {
+            console.log("Email Sent: " + info.response);
+            res.render("success");
+        }
+    });
+
+
+});
 
 
 
